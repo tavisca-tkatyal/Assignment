416672af9b54b24f15d82a8d12495103a50e3273
tavisca-tkatyal <tkatyal@tavisca.in>
2019-06-27 19:03:14 +0530

    Add method in file2.rb

--- lib/file2.rb
+++ lib/file2.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 require 'find'
 require 'pathname'
-print "Hello user, Please Input the folder name"
+puts "Hello user, Please Input the folder name"
 name = gets.chomp
-dirsize = 0
-Find.find(name) do |f| dirsize += File.stat(f).size end
-dirsize
-puts dirsize
+def folderSize(name)
+	dirsize = 0
+	Find.find(name) do |f| dirsize += File.stat(f).size end
+	dirsize
+	puts dirsize
+end
+folderSize(name)
