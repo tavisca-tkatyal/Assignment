c7d4ec5f57880a8dc9f2f7b91ac224178c08ef8d
tavisca-tkatyal <tkatyal@tavisca.in>
2019-06-29 20:03:38 +0530

    Add user input in method

--- lib/file2.rb
+++ lib/file2.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 require 'find'
 require 'pathname'
-puts "Hello user, Please Input the folder name"
-name = gets.chomp
-def folderSize(name)
+def folderSize()
+	puts "Hello user, Please Input the folder name"
+	name = gets.chomp
 	dirsize = 0
 	Find.find(name) do |f| dirsize += File.stat(f).size end
 	dirsize
 	puts dirsize
 end
-folderSize(name)
+folderSize()
